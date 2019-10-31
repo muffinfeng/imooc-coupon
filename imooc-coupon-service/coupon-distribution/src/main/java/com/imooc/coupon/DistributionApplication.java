932da10e90b2987cfc37351eda4f31bf8b528c18
muffin <1697426994@qq.com>
2019-10-31 10:20:43 -0700

    优惠卷系统（第一次修改）

---
+++ imooc-coupon-service/coupon-distribution/src/main/java/com/imooc/coupon/DistributionApplication.java
@@ -0,0 +1,22 @@
+package com.imooc.coupon;
+
+//分发系统的启动入口
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class DistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionApplication.class,args);
+    }
+}
