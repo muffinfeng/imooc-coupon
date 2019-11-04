e44ad9111512611a5318c90c9fb64dd4f093653e
muffin <1697426994@qq.com>
2019-11-04 06:34:06 -0800

    11/4

--- imooc-coupon-service/coupon-distribution/src/main/java/com/imooc/coupon/DistributionApplication.java
+++ imooc-coupon-service/coupon-distribution/src/main/java/com/imooc/coupon/DistributionApplication.java
@@ -5,16 +5,26 @@ package com.imooc.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
-@EnableJpaAuditing
-@EnableFeignClients
-@EnableCircuitBreaker
-@EnableEurekaClient
+@EnableJpaAuditing   //jpa填充数据列
+@EnableFeignClients  //调用其他微服务
+@EnableCircuitBreaker  //融断降级
+@EnableEurekaClient  //作为一个client
 @SpringBootApplication
 public class DistributionApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DistributionApplication.class,args);
