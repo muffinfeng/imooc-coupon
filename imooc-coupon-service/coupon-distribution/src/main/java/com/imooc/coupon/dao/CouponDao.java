6f694b7828aa62b76ce506a1236f92e35d9b3696
muffin <1697426994@qq.com>
2019-11-05 07:57:35 -0800

    11/5

--- imooc-coupon-service/coupon-distribution/src/main/java/com/imooc/coupon/dao/CouponDao.java
+++ imooc-coupon-service/coupon-distribution/src/main/java/com/imooc/coupon/dao/CouponDao.java
@@ -1,4 +1,16 @@
 package com.imooc.coupon.dao;
 
-public class CouponDao {
+import com.imooc.coupon.constant.CouponStatus;
+import com.imooc.coupon.entity.Coupon;
+
+import java.util.List;
+
+public interface CouponDao {
+
+    /**
+     * <h2>根据 userId + 状态寻找优惠券记录</h2>
+     * where userId = ... and status = ...
+     * */
+    List<Coupon> findAllByUserIdAndStatus(Long userId, CouponStatus status);
+
 }
